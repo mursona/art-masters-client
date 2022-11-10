cb92d097ede8bf42950d51d90b994f452306e2b2
mursona <mursona>
2022-11-10 23:34:09 +0600

    some correction

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -83,7 +83,7 @@ if(loading){
                 <label className="label">
                     <span className="label-text">Password</span>
                 </label>
-                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                 <label className="label">
                     <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
                 </label>
